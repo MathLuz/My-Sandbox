2cd8b39d91edacc99802dcfa27840d87bee599df
matheus.fernandes <matheus.fernandes@lbv.org.br>
2024-09-13 17:47:24 -0300

    Ajustes

--- chuva/script.js
+++ chuva/script.js
@@ -1,5 +1,11 @@
 const volumeInverso = 12; // Volume de gotas - quanto menor mais gotas
-const tempo = 4;         // Tempo de passagem - quanto menor mais rápido
+const root = document.documentElement;
+// root.style.setProperty('--topRain', (window.innerHeight * 2) + 'px');
+
+// let tempo = window.innerHeight / 500;
+const tempo = 5;
+
+// const rootCss = document.documentElement;
 
 function posicoesAleatorias(tamanho) {
     // console.log("Mudou " + tamanho);
@@ -14,50 +20,97 @@ function posicoesAleatorias(tamanho) {
         };
         elemento.style.boxShadow = shadowBox;
     });
-} function numeroAleatorioX() {
-    let max = window.innerWidth;
-    let min = 0;
+}
+function numeroAleatorioX() {
+    const max = window.innerWidth * 1.1;
+    const min = 0;
     // console.log(max)
     return Math.random() * (max - min) + min;
-} function numeroAleatorioY() {
-    // let max = window.innerHeight * 1.5;
-    let max = 2000;
-    let min = 0;
+}
+function numeroAleatorioY() {
+    const max = window.innerHeight;
+    // const max = 2000;
+    const min = 0;
     // console.log(window.innerHeight)
     return Math.random() * (max - min) + min;
 }
-const tempoS = (tempo * 1000);
-const tempoM = (tempo * 1000) / 1.5;
-const tempoL = (tempo * 1000) / 2;
 
-const root = document.documentElement;
-root.style.setProperty('--speedS', tempoS / 1000 + 's');
-root.style.setProperty('--speedM', tempoM / 1000 + 's');
-root.style.setProperty('--speedL', tempoL / 1000 + 's');
+let tempoS = (tempo * 1000);
+let tempoM = (tempo * 1000) / 1.5;
+let tempoL = (tempo * 1000) / 2;
+
+definirTempoRoot()
+function definirTempoRoot() {
+    root.style.setProperty('--speedS', tempoS / 1000 + 's');
+    root.style.setProperty('--speedM', tempoM / 1000 + 's');
+    root.style.setProperty('--speedL', tempoL / 1000 + 's');
+}
+
 
 let alternS = false;
+let alternM = false;
+let alternL = false;
+
 choveS();
+choveM();
+choveL();
+
 setInterval(() => choveS(), tempoS / 2);
+setInterval(() => choveM(), tempoM / 2);
+setInterval(() => choveL(), tempoL / 2);
+
 function choveS() {
     posicoesAleatorias(`small ${alternS ? 'delay' : 'start'}`);
     alternS = alternS ? false : true;
 }
-let alternM = false;
-choveM();
-setInterval(() => choveM(), tempoM / 2);
+
 function choveM() {
     posicoesAleatorias(`medium ${alternM ? 'delay' : 'start'}`);
     alternM = alternM ? false : true;
 }
-let alternL = false;
-choveL();
-setInterval(() => choveL(), tempoL / 2);
+
 function choveL() {
     posicoesAleatorias(`large ${alternL ? 'delay' : 'start'}`);
     alternL = alternL ? false : true;
 }
 
+/*
+function resetTempoChuva() {
+    clearInterval(intervalS);
+    clearInterval(intervalM);
+    clearInterval(intervalL);
+
+    console.log(tempoS)
+    choveS();
+    choveM();
+    choveL();
+}
+*/
+
+/*
+function aoMudarZoom() {
+root.style.setProperty('--topRain', (window.innerHeight * 2) + 'px');
+    tempo = window.innerHeight / 500;
+    tempoS = (tempo * 1000);
+    tempoM = (tempo * 1000) / 1.5;
+    tempoL = (tempo * 1000) / 2;
+
+    resetTempoChuva()
+    definirTempoRoot()
+}
+*/
+// aoMudarZoom();
+
+
+/*
+window.addEventListener('resize', () => {
+    // console.log(window.innerHeight)
+    aoMudarZoom();
+});
+*/
+
 // AUDIO
+
 window.onload = () => sonsDeChuva();
 function sonsDeChuva() {
     const audio = new Audio('audio/serene-rain.mp3');
